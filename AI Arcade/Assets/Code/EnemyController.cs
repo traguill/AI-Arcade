fdd334d8a00ecad5a2061286b1de6529e20f6b44
traguill <traguill1@gmail.com>
2016-09-22 18:20:41 +0200

    Exercise 1

---
+++ AI Arcade/Assets/Code/EnemyController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour 
+{
+    NavMeshAgent agent;
+
+    public GameObject player;
+    PlayerController player_controller;
+    public float escape_dst = 5.0f;
+
+    bool follow = true;
+    
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        player_controller = player.GetComponent<PlayerController>();
+    }
+
+    
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            follow = !follow;
+            if (follow == false)
+            {
+                agent.speed = 5.0f;
+                agent.updatePosition = false;
+            }
+                
+            else
+            {
+                agent.speed = 3.5f;
+                agent.Warp(transform.position);
+                agent.updatePosition = true;
+            }
+                
+        }
+
+        if(follow)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            Vector3 distance = transform.position - player.transform.position;
+            distance.y = 0;
+            if(distance.magnitude < escape_dst)
+            {
+                transform.position += (distance.normalized * agent.speed) * Time.deltaTime;
+            }
+        }
+
+            
+
+     
+	}
+}
---
+++ AI Arcade/Assets/Code/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 3.5f;
+
+    Vector3 speed_vec = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        speed_vec.Set(0, 0, 0);
+
+	    if(Input.GetKey(KeyCode.RightArrow))
+        {
+             speed_vec.x = Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed_vec.x = Time.deltaTime * -speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed_vec.z = Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            speed_vec.z = Time.deltaTime * -speed;
+        }
+
+        transform.position += speed_vec;
+	}
+
+
+  
+}
